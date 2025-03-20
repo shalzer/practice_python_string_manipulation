9ceb3734676cc8229e19999fe7671d74bbf77e9f
KER AND HAN <shalcozbi@gmail.com>
2025-03-20 22:56:31 +0800

    pseudocode batch5 prog4

---
+++ batch_5/print_lower_case.py
@@ -0,0 +1,5 @@
+#Prog04: Create a program that ask the user to input their fullname. Print the input in lower case.
+
+#Ask user to enter full name
+#correct the format using .lower
+#print full name in lower case
