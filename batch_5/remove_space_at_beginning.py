2dce9bb142ce8a183abca2d20fad1f94a13f25da
KER AND HAN <shalcozbi@gmail.com>
2025-03-20 22:52:19 +0800

    corrected variable error

--- batch_5/remove_space_at_beginning.py
+++ batch_5/remove_space_at_beginning.py
@@ -3,5 +3,6 @@
 #Ask the user to enter their full name with extra space at beginning
 #Print their full name without space
 
-user_name = input("Enter your full name with space at the front: ").lstrip()
-print(user_name)
+name = input("Enter your full name with space at the front: ")
+NAME = name.strip()
+print(NAME)
