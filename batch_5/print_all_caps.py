6249c24384b5800c08945085a6522871579aff8f
KER AND HAN <shalcozbi@gmail.com>
2025-03-20 22:47:26 +0800

    pseudocode batch 5 prog3

---
+++ batch_5/print_all_caps.py
@@ -0,0 +1,5 @@
+#Prog03: Create a program that ask the user to input their fullname. Print the input in all capital letter.
+
+#Ask user to enter full name
+#correct the format using .upper
+#print full name in all capital letters
