d7025aaaf649a5c6a329e46f48bfd16c40b40c9e
KER AND HAN <shalcozbi@gmail.com>
2025-03-20 22:36:57 +0800

    pseudocode batch5 prog2

---
+++ batch_5/print_6_digit_format.py
@@ -0,0 +1,5 @@
+#Prog02: Create a program that ask the user to input a number (0-1000). Print the number in 6 digit format. Add zeros at the beginning to complete the 6 digit.
+
+#Ask user to enter a number from 0-1000
+#Using f-string, change the format of entered number into 6-digit format adding zeros to complete it.
+#print the number in correct format
