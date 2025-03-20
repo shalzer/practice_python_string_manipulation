cfee991a89bf76de3af7ac3d9707e4611b3689ab
KER AND HAN <shalcozbi@gmail.com>
2025-03-20 22:13:54 +0800

    pseudocode batch 5 prog1

---
+++ batch_5/remove_space_at_beginning.py
@@ -0,0 +1,4 @@
+#Prog01: Create a program that ask the user to input their fullname with several space characters at the beginning. Print the input without the spaces in the beginning.
+
+#Ask the user to enter their full name with extra space at beginning
+#Print their full name without space
