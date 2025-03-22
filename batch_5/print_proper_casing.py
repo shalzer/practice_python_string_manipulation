848cd0f808ad3dadaf22c60890b5fd96c251f265
KER AND HAN <shalcozbi@gmail.com>
2025-03-22 12:37:29 +0800

    pseudocode prog 5

---
+++ batch_5/print_proper_casing.py
@@ -0,0 +1,4 @@
+# Prog05: Create a program that ask the user to input their fullname in incorrect casing. Print the input in proper casing.
+
+#Ask user to enter their full name in incorrect casing
+#Print their full name in proper casing
