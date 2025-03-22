a1c609c91e31ad240ab7b0894bc5b1bd5ec20df9
KER AND HAN <shalcozbi@gmail.com>
2025-03-22 12:41:23 +0800

    pseudocode prog 6

---
+++ batch_5/print_reverse_casing.py
@@ -0,0 +1,4 @@
+# Prog06: Create a program that ask the user to input their fullname in incorrect casing. Print each character of the input in reverse casing.
+
+#Ask user to input their full name in incorrect casing
+#Print their full name in reverse casing
