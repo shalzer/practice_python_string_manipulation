38dceb71136ba252aee55c630260ff44e9677aa7
KER AND HAN <shalcozbi@gmail.com>
2025-03-22 12:45:03 +0800

    pseudocode prog 9

---
+++ batch_5/print_pascal_case.py
@@ -0,0 +1,4 @@
+# Prog09: Create a program that ask the user to input their fullname in incorrect casing. Print the input in pascal case.
+
+#Ask user to entr their full name in incorrect casing
+#Print full name in pascal case
